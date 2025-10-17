dbda87dcbf82cc72012c7fff3064f7e340dcc6a1
Marcos <marcospaulomk09@gmail.com>
2025-10-17 20:38:20 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Black;
+string senhaDigitada;
+string senhaCorreta = "123abc";
+
+Console.Write("Digite sua Senha : ");
+senhaDigitada = Console.ReadLine()!;
+
+bool acertou = senhaDigitada == senhaCorreta;
+
+if(acertou)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Senha Correta!!!");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("ERROOOOOUUUUUUU!!!");
+};
+Console.ResetColor();
